383d3b2a76de8775f6a77377b6b38eddb519ad82
VincentRQ <VRQ20@fsu.edu>
2024-02-02 19:05:14 -0500

    Web Scraping - Started on Portfolio Valuation with Google Finance
    Started work on parsing price & non-USD prices; also worked on price conversion.

--- Beautiful Soup/Learning 1/BS.py
+++ Beautiful Soup/Learning 1/BS.py
@@ -1,17 +1,41 @@
-
 import requests as r
 from bs4 import BeautifulSoup
 
-resp = r.get("https://books.toscrape.com/")
+# https://www.google.com/finance/quote/GOOGL:NASDAQ
 
-resp.content
+def get_fx_to_usd(currency):
+    fx_url = f"https://www.google.com/finance/quote/{currency}-USD"
+    resp = r.get(fx_url)
+    soup = BeautifulSoup(resp.content, "html.parser")
+
+    fx_rate = soup.find("div", attrs= {"data-last-price": True}) # Here, we are telling it which div includes the data-last-price
+    fx = float(fx_rate['data-last-price']) # Here, we are telling Python what data point we actually want.
+    return fx
+
+def get_price_information(ticker, exchange):
+    url = f"https://www.google.com/finance/quote/{ticker}:{exchange}"
+    resp = r.get(url)
+    soup = BeautifulSoup(resp.content, "html.parser")
+
+    price_div= soup.find("div", attrs= {"data-last-price": True})
+    price = float(price_div["data-last-price"])
+    currency = price_div["data-currency-code"]
+
+    usd_price = price
+    if currency != "USD":
+        fx = get_fx_to_usd(currency)
+        usd_price = round(price * fx, 2)
 
 
-soup = BeautifulSoup(resp.content, "html.parser") #lxml - bit outdated, but works for our purposes
+    return {
+        "ticket":ticker,
+        "exchange": exchange,
+        "price": price,
+        "currency": currency,
+        "usd_price": usd_price
+    }
 
-type(soup)
-
-soup.title
-
-
-
+if __name__ == "__main__":
+    # print(get_price_information("AMZN", "NASDAQ"))
+    # print(get_price_information("SHOP", "TSE"))
+    print(get_fx_to_usd("CAD"))
