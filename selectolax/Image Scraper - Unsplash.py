1fa545906273b0cfe02b4810381666ec91bb4702
VincentRQ <VRQ20@fsu.edu>
2024-03-12 14:44:56 -0400

    Update Image Scraper - Unsplash.py
    Added logging & saving features to local directory.
    
    One issue remains with the debugging - specifically with the MB shown.

--- selectolax/Image Scraper - Unsplash.py
+++ selectolax/Image Scraper - Unsplash.py
@@ -1,5 +1,9 @@
 from httpx import get
 from selectolax.parser import HTMLParser
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(asctime)s - %(levelname)s - %(message)s")
 
 def get_img_tags_for(term=None):
     if not term:
@@ -33,16 +37,36 @@ def get_high_reso_img_url(img_node):
     return url_res[0][0].split("?")[0]
 
 
+import os
 
+def save_images(img_urls, dest_dir="images", tag=""):
+    for url in img_urls:
+        resp = get(url)
+        logging.info(f"Downloading {url}...")
+
+        file_name = url.split("/")[-1]
+
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+
+        with open(f"{dest_dir}/{tag}{file_name}.jpeg", "wb") as f:
+            f.write("resp.content")
+            logging.info(f"Saved {file_name}, with size {round(len(resp.content)/1024/1024,2)} MB.")
 
 
 
 if __name__ == '__main__':
-    img_nodes = get_img_tags_for('galaxy')
+    search_tag = "dolphin"
+    dest_dir = "dolphins"
+
+
+
+    img_nodes = get_img_tags_for(search_tag)
     all_img_urls = [get_high_reso_img_url(i) for i in img_nodes]
     img_urls = [u for u in all_img_urls if u]
+    save_images(img_urls[:3], "images", search_tag)
 
-    print(img_urls)
+    # print(img_urls)
     # [print(get_high_reso_img_url(i)) for i in img_nodes[:4]]
 
     # img_urls = [i.attrs["src"] for i in img_nodes]
