40c1f9ce0f4c8d114a4232de67baba67ccd1b8d1
VincentRQ <VRQ20@fsu.edu>
2024-03-17 16:30:53 -0400

    Update Steam - Javascript Scraping with PlayWright.py
    Worked on more robust render thresholds, extracting title, thumbnail, and game category tags.
    
    Issue is that the steam HTML has changed, making the process difficult for obtaining the title needed - unsure if this is due to a delay (which DOM should be loaded before scraping), if the class attribute is dynamically generated (which is probably the case), or something else I am not seeing. Will try fixing at future date.

--- Microsoft Playwrite/Steam - Javascript Scraping with PlayWright.py
+++ Microsoft Playwrite/Steam - Javascript Scraping with PlayWright.py
@@ -13,15 +13,42 @@ if __name__ == "__main__":
         page.wait_for_load_state("networkidle")
         page.evaluate("() => window.scroll(0,document.body.scrollHeight)")
         page.wait_for_load_state('domcontentloaded')
+        page.wait_for_selector('div[class*="StoreSalePriceWidgetContainer"]')
 
 
         # page.screenshot(path="steam2.png", full_page=True)
-        html = page.inner_html('body')
+        html = page.inner_html("body")
         tree = HTMLParser(html)
 
         # divs = tree.css('div[class*="salepreviewwidgets_StoreSaleWidgetContainer"]') pre-Steam website change
         divs = tree.css('div[class*="StoreSalePriceWidgetContainer"]')
+
+
+
         print(len(divs))
 
         for d in divs:
-            print(d)
+            # title = d.css_first('div[class*="StoreSaleWidgetTitle"]').text()
+            # print(title)
+
+            title = d.css_first('div[class*="StoreSaleWidgetTitle"]')
+            if title is not None:
+                title = title.text()
+                print(title)
+            else:
+                print("Title not found")
+            # thumbnail = d.css_first('img[class*="CapsuleImage"]').attributes.get("src")
+            tags = [a.text() for a in d.css("div[class*='_3OSJsO_BdhSFujrHvCGLqV'] > a")[:5]]
+
+            attrs = {
+                "title": title,
+                "tags": tags,
+                # "thumbnail": thumbnail
+            }
+
+            print(attrs)
+
+
+
+
+
