03fe4c32c07d6cd558c7db635458ef0534ed369d
VincentRQ <VRQ20@fsu.edu>
2024-03-20 11:54:32 -0400

    Somewhat Minor Changes
    Worked with the scrapy shell, rewriting using XPath Selectors (an introduction to Xpath/XML), and outputting data using scrapy

--- ExploringScrapy/countries_gdp/countries_gdp/spiders/gdp.py
+++ ExploringScrapy/countries_gdp/countries_gdp/spiders/gdp.py
@@ -8,12 +8,35 @@ class GdpSpider(scrapy.Spider):
 
     def parse(self, response):
         # table.wikitable.sortable
-        for country in response.css('table.wikitable.sortable tbody tr'):
+        for country in response.css('table.wikitable.sortable tbody tr:not([class])'):
             yield {
                 "country_name": country.css('td:nth-child(1) a::text').get(),
                 "region": country.css('td:nth-child(2) a::text').get(),
                 "gdp": country.css('td:nth-child(3) ::text').get(),
-                "year": country.css('td:nth-child(4) ::text').get()
-
+                "year": country.css('td:nth-child(4) ::text').get(),
+                #we can query various things using the scrapy shell selector.
             }
+        # for country in response.xpath("//table[contains(@class,'wikitable sortable')]//tbody//tr"):
+        #     yield {
+        #         "country_name": country.xpath(".//td[1]//a/text()").get(),
+        #         "region": country.xpath(".//td[2]//a/text()").get(),
+        #         "gdp": country.xpath(".//td[3]/text()").get(),
+        #         "year": country.xpath(".//td[4]/text()").get(),
+        #
+        #     }
+            #using xpath
+            # /html/body/table[3]/tbody/tr/td[2]
+            # //table[3]//td[2]
+            # //table[@class='wikitable']
+            # //table[3]/@closs
+            # //img[1]/@src
+            # //table[3]/td[2]/text()
+
+            #for outputting: scrapy crawl gdp -O  gdp.csv (allows for overwriting with capital 0)
+
+
+
+
+
+
 
