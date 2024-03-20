f4bddcb2e7404b5e5779846a571995e243b8aa39
VincentRQ <VRQ20@fsu.edu>
2024-03-20 13:23:52 -0400

    Large Update
    Worked with Scrapy Itemloaders, worked to fine-tune post-process data, pipelined data validation, and created a database with sqlite & integrated it with the current data extraction method.

--- ExploringScrapy/countries_gdp/countries_gdp/spiders/gdp.py
+++ ExploringScrapy/countries_gdp/countries_gdp/spiders/gdp.py
@@ -1,4 +1,6 @@
 import scrapy
+from countries_gdp.items import CountryGdpItem
+from scrapy.loader import ItemLoader
 
 
 class GdpSpider(scrapy.Spider):
@@ -9,13 +11,23 @@ class GdpSpider(scrapy.Spider):
     def parse(self, response):
         # table.wikitable.sortable
         for country in response.css('table.wikitable.sortable tbody tr:not([class])'):
-            yield {
-                "country_name": country.css('td:nth-child(1) a::text').get(),
-                "region": country.css('td:nth-child(2) a::text').get(),
-                "gdp": country.css('td:nth-child(3) ::text').get(),
-                "year": country.css('td:nth-child(4) ::text').get(),
+            # item = CountryGdpItem()
+            # item["country_name"] = country.css('td:nth-child(1) a::text').get()
+            # item["region"] = country.css('td:nth-child(2) a::text').get()
+            # item["gdp"] = country.css('td:nth-child(3) ::text').get()
+            # item["year"] = country.css('td:nth-child(4) ::text').get()
+            # yield item
                 #we can query various things using the scrapy shell selector.
-            }
+            item = ItemLoader(item=CountryGdpItem(), selector=country)
+            item.add_css("country_name", 'td:nth-child(1) a')
+            #We are using takefirst, so we don't need to use the ::text after the anchor.
+            item.add_css("region", 'td:nth-child(2) a')
+            item.add_css("gdp", 'td:nth-child(3)')
+            item.add_css("year", 'td:nth-child(4)')
+
+            yield item.load_item()
+
+
         # for country in response.xpath("//table[contains(@class,'wikitable sortable')]//tbody//tr"):
         #     yield {
         #         "country_name": country.xpath(".//td[1]//a/text()").get(),
